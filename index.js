d96c5482ea3fc6a7cea9f480b80c12d859a68882
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-04-26 19:48:05 -0700

    commit tree 389285b7eae518d485adb95d0a874f25f08be460

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(num){
+  if (num <= 400){
+    return 'This one is on me!';
+  }
+  else if (num <= 2000){
+    return 'That will be twenty bucks.';
+  }
+  else if(num <= 2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else{
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const newCity = (city == 'NYC') ? 'Ok, sounds good.' : 'No go.' ;
+  return newCity; 
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  const isTipGenerous = tip;
+
+  switch (isTipGenerous){
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default: 
+      return 'Bye.';
+  }
 }
